99894e51711086d1aa21fec655f56f7f257dc1ef
Chantel Lowe <chantellowe17@gmail.com>
2019-08-03 13:03:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,11 +26,7 @@ def find_the_cheese(array) # code an argument here
   
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  if array.join(" ").any? {|cheese| cheese.include?(cheese_types)} == false
-    return nil
-  else
-    return array[cheese]
-  end
+  array.join(" ")
   
 end
 
