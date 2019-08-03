e8ea544332a72f7a7605b6b3b265a2f004fdc29a
Chantel Lowe <chantellowe17@gmail.com>
2019-08-03 12:33:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 def summon_captain_planet(planeteer_calls) # code an argument here
   # Your code here
   
-  planeteer_calls
+  planeteer_calls.collect { |word| word.capitalize + "!" }
   
 end
 
