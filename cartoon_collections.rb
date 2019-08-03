6fd8535c9f68441493730fbc3dfd85a154ab3385
Chantel Lowe <chantellowe17@gmail.com>
2019-08-03 13:20:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_the_cheese(array) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   #array.collect.join(" ")
-  array.collect.any?(cheese_types)
+  array.collect.any? {cheese_types}
     
   
 end
